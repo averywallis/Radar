a5304936b816863a0c74a3901da9305986060c13
averywallis <averybwallis@gmail.com>
2016-11-08 13:25:21 -0500

    Updated from Brython Server: 11/8/2016 1:25:21 PM

--- FinalProject-Portal.py
+++ FinalProject-Portal.py
@@ -57,7 +57,7 @@ class OceanDepth(App):
         super().__init__(width, height)
         sonar((0,0))
     def step(self):
-        for sonar in self.getSpritesbyClass(sonar):
+        for thing in self.getSpritesbyClass(sonar):
             sonar.step()
             
 myapp = OceanDepth(1000,750)
