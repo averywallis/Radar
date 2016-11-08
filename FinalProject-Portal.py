5aac0314f1c3401983e2ab6645498d638cfdcf90
averywallis <averybwallis@gmail.com>
2016-11-08 13:18:39 -0500

    Updated from Brython Server: 11/8/2016 1:18:39 PM

--- FinalProject-Portal.py
+++ FinalProject-Portal.py
@@ -47,8 +47,8 @@ class sonar(Sprite):
     asset = RectangleAsset(5,5,thickline,darkblue)
     def __init__(self, position):
         super().__init__(sonar.asset, position)
-        self.x = 10
-        self.y = 10
+        self.x = 0
+        self.y = 0
     def step(self):
         self.x += 10
         self.x = self.x
