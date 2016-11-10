101144c6bb8b0923e3fa9a12c932785220145679
averywallis <averybwallis@gmail.com>
2016-11-10 12:06:56 -0500

    Updated from Brython Server: 11/10/2016 12:06:55 PM

--- COMAPTriathlon.py
+++ COMAPTriathlon.py
@@ -300,18 +300,34 @@ class fop(Sprite):
         self.t = time.time()
         self.a = 0
     def step(self):
-        if self.p==1:
+        if self.p == 1:
             self.x += fops
-            if self.x >= 55+(945/51.5):
+            if self.x >= ((SCREEN_WINDOWX - startpos)/td)*1.5:
+                self.p = 0
+        if self.p == 2:
+            self.x += fopb
+            if self.x >= ((SCREEN_WINDOWX - startpos)/td)*41.5:
+                self. p = 0
+        if self.p == 4:
+            self.x += fopr
+            if self.x >= ((SCREEN_WINDOWX - startpos)/td)*51.5:
                 self.p = 0
         if self.p == 0 and self.a == 0:
             self.x += 0
             self.a = 1
             self.t = time.time()
+        elif self.p == 0 and self.a == 2:
+            self.x += 0
+            self.a = 3
+            self.t= time.time()
+        elif self.p == 0 and self.a == 4:
+            self.x += 0
         if time.time() >= self.t + fopt1 and self.p == 0 and self.a == 1:
                 self.p = 2
-        if self.p == 2:
-            self.x += fopb
+                self.a = 2
+        if time.time() >= self.t + fopt2 and self.p == 0 and self.a == 3:
+                self.p = 4
+                self.a = 4
         
 class cyl(Sprite):
     def __init__(self, position):
