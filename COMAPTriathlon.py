4654f1d1870d2c2a7dfc648e313d4ad86163ca1c
averywallis <averybwallis@gmail.com>
2016-11-10 09:19:29 -0500

    Updated from Brython Server: 11/10/2016 9:19:29 AM

--- COMAPTriathlon.py
+++ COMAPTriathlon.py
@@ -90,14 +90,14 @@ portalline= LineStyle(1, blue)
 portalline2= LineStyle(1, orange)
 
 class start(Sprite):
-    asset = ImageAsset("images/spritesforathletes.jpg", Frame(0,0,55,50), 1, 'veritcal')
+    asset = ImageAsset("images/spritesforathletes.jpg", Frame(0,0,55,50), 8, 'horizontal')
     def __init__(self, position):
         super().__init__(start.asset, position)
         self.vx=0
         self.vy=0
         
 class finish(Sprite):
-    asset = ImageAsset("images/spritesforathletes.jpg", Frame(0,0,55,50), 2, 'veritcal')
+    asset = ImageAsset("images/spritesforathletes.jpg", Frame(0,0,55,50), 1, 'veritcal')
     def __init__(self, position):
         super().__init__(finish.asset, position)
         self.vx=0
