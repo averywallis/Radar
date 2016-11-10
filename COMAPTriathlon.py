b9c75372e1e3c2c0474a6eff39a11ddf2f1084f5
averywallis <averybwallis@gmail.com>
2016-11-10 11:13:19 -0500

    Updated from Brython Server: 11/10/2016 11:13:19 AM

--- COMAPTriathlon.py
+++ COMAPTriathlon.py
@@ -140,7 +140,7 @@ class fpro(Sprite):
             self.x += 0
             self.a = 1
             self.t = time.time()
-        if time.time() >= self.t + mprot1 and self.p == 0 and self.a == 1:
+        if time.time() >= self.t + fprot1 and self.p == 0 and self.a == 1:
                 self.p = 2
         if self.p == 2:
             self.x += fprob
