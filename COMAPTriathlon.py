7bc71ed02ecf00fa0c3ed245ad5d1d7d44c67e0a
averywallis <averybwallis@gmail.com>
2016-11-10 12:37:17 -0500

    Updated from Brython Server: 11/10/2016 12:37:16 PM

--- COMAPTriathlon.py
+++ COMAPTriathlon.py
@@ -422,7 +422,7 @@ class triathlon(App):
         transtobike = TextAsset(text="Transition to Biking", width=200, align='center',style='10px Arial', fill=Color(0xff2222,1))
 
         Sprite(start,(startpos-10,500))
-        Sprite(transtobike,(s,500))
+        Sprite(transtobike,(startpos+s,500))
         mpro((0,0))
         fpro((0,50))
         mpre((0,100))
