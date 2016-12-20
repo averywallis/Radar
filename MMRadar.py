da1b4455f193ac9b671a163d6026c88efbfbbc9c
averywallis <averybwallis@gmail.com>
2016-12-19 22:06:27 -0500

    Updated from Brython Server: 12/19/2016 10:06:28 PM

--- MMRadar.py
+++ MMRadar.py
@@ -105,10 +105,10 @@ class radar(App):
     def __init__(self, width, height):
         super().__init__(width, height)
         Sprite(RectangleAsset(1000,500,thinlinesb,skyblue),(0,0))
+        fog((100,100))
         dish((100,450))
         signal((102,450))
         plane((900,100))
-        fog((100,100))
         for x in range(0,41):
             rain((random.randrange(400,800),random.randrange(0,500)))
         Sprite(LineAsset(1000,1,thinline),(0,500))
