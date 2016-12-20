ebcc3f07979ca240ade3ed2312f3869782e29433
averywallis <averybwallis@gmail.com>
2016-12-19 19:41:26 -0500

    Updated from Brython Server: 12/19/2016 7:41:26 PM

--- MMRadar.py
+++ MMRadar.py
@@ -40,7 +40,7 @@ class dish(Sprite):
     def __init__(self, position):
         super().__init__(dish.asset, position)
         self.vx=0
-        self.rotation=-0.8
+        self.rotation=-0.9
         
 class signal(Sprite):
     asset = EllipseAsset(50,5,thinline,white)
