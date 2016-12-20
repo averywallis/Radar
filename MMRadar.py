028f8989bebf791b47318b543f16e3a3b055e382
averywallis <averybwallis@gmail.com>
2016-12-19 19:59:59 -0500

    Updated from Brython Server: 12/19/2016 8:00:00 PM

--- MMRadar.py
+++ MMRadar.py
@@ -51,7 +51,7 @@ class signal(Sprite):
         self.vx=0
         self.rotation=-1
     def step(self):
-        
+        self.x += 1
         
 class plane(Sprite):
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
