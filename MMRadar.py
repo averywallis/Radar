192c140d440fd1435269f14abc35b623fe4415ac
averywallis <averybwallis@gmail.com>
2016-12-19 21:46:25 -0500

    Updated from Brython Server: 12/19/2016 9:46:26 PM

--- MMRadar.py
+++ MMRadar.py
@@ -97,7 +97,7 @@ class radar(App):
         signal((102,450))
         plane((900,100))
         for x in range(0,11):
-            rain((random.randrange(100,200),random.randrange(100,200)))
+            rain((random.randrange(400,500),random.randrange(100,200)))
         rain((100,100))
         Sprite(LineAsset(1000,1,thinline),(0,500))
     def step(self):
