eb6eca639ff72c385f7d45e7d000e24e8df4c9ec
averywallis <averybwallis@gmail.com>
2016-12-19 22:05:37 -0500

    Updated from Brython Server: 12/19/2016 10:05:38 PM

--- MMRadar.py
+++ MMRadar.py
@@ -104,7 +104,7 @@ class radar(App):
     asset = ImageAsset("images/spritesforathletes.jpg", Frame(0,0,55,50), 8, 'horizontal')
     def __init__(self, width, height):
         super().__init__(width, height)
-        Sprite(RectangleAsset(1000,1000,thinlinesb,skyblue),(0,0))
+        Sprite(RectangleAsset(1000,500,thinlinesb,skyblue),(0,0))
         dish((100,450))
         signal((102,450))
         plane((900,100))
