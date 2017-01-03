7613238a1557bfbaa58e6724dd0da0cb6f82b332
averywallis <averybwallis@gmail.com>
2017-01-03 13:54:32 -0500

    Updated from Brython Server: 1/3/2017 1:54:32 PM

--- MMRadar.py
+++ MMRadar.py
@@ -129,7 +129,7 @@ class signal(Sprite):
         self.i = 0
     def step(self):
         if self.a == 0:
-            self.i += .001
+            self.i += .01
             self.setImage(self.i)
             self.x += 3*speed
             self.y -= 1.5*speed
