b6db495febd8e763b11af67af3d688839ea633db
averywallis <averybwallis@gmail.com>
2017-01-03 13:57:10 -0500

    Updated from Brython Server: 1/3/2017 1:57:09 PM

--- MMRadar.py
+++ MMRadar.py
@@ -123,7 +123,7 @@ class signal(Sprite):
         super().__init__(signal.asset, position)
         self.vx=0
         self.vy=0
-        self.scale = .4
+        self.scale = .45
         self.rotation=.5
         self.a = 0
         self.i = 0
