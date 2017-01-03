e77c795499354f922d6af63640ec2c14128d371e
averywallis <averybwallis@gmail.com>
2017-01-03 13:58:57 -0500

    Updated from Brython Server: 1/3/2017 1:58:57 PM

--- MMRadar.py
+++ MMRadar.py
@@ -143,8 +143,8 @@ class signal(Sprite):
             if self.x <= 102:
                 self.a = 2
         if self.a == 2:
-            self.x = 75
-            self.y = 410
+            self.x = 70
+            self.y = 420
 class plane(Sprite):
     asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
         Frame(227,0,292-227,125), 4, 'vertical')
