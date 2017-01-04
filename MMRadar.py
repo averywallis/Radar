dffbc58d8262884cb85c677a12e76c5bf967ef8c
averywallis <averybwallis@gmail.com>
2017-01-04 12:55:43 -0500

    Updated from Brython Server: 1/4/2017 12:55:42 PM

--- MMRadar.py
+++ MMRadar.py
@@ -149,7 +149,7 @@ class signal(Sprite):
             if self.i >= 30:
                 self.setImage(29)
                 self.a = 2
-                Sprite(TextAsset(text="Radar Dish", width = 200, align = 'center', style = '10px Arial', fill=black),(0,0))
+                Sprite(TextAsset(text="Signal Is Not Strong Enough", width = 200, align = 'center', style = '120px Arial', fill=black),(0,0))
             if self.x <= 72:
                 self.a = 2
         if self.a == 2:
