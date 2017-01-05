8b9b1a007b55111ba6ee791d9911d2f70faeab1a
averywallis <averybwallis@gmail.com>
2017-01-05 13:47:42 -0500

    Updated from Brython Server: 1/5/2017 1:47:41 PM

--- MMRadar.py
+++ MMRadar.py
@@ -97,7 +97,7 @@ portalline= LineStyle(1, blue)
 portalline2= LineStyle(1, orange)
 
 class dish(Sprite):
-    asset = EllipseAsset(50,5,thin1line,white)
+    asset = EllipseAsset(5,50,thin1line,white)
     def __init__(self, position):
         super().__init__(dish.asset, position)
         self.vx=0
