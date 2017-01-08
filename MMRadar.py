7ee394da492c3f0735c3da296127a1465ebf170d
averywallis <averybwallis@gmail.com>
2017-01-08 17:02:11 -0500

    Updated from Brython Server: 1/8/2017 5:02:13 PM

--- MMRadar.py
+++ MMRadar.py
@@ -166,8 +166,8 @@ class signal(Sprite):
                 self.setImage(0)
             #self.x += 3*speed
             #self.y -= 1.5*speed
-            self.x += ((cpx-100)/100)*(self.speed/100)
-            self.y -= ((450-cpy)/100)*(self.speed/100)
+            self.x += ((cpx-100)/100)*(self.speed)
+            self.y -= ((450-cpy)/100)*(self.speed)
             if self.x >= cpx:
                 self.a = 1
                 if self.i >= 0:
