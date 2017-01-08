731124c6c9235ec5c18169460ce170c76b8c058e
averywallis <averybwallis@gmail.com>
2017-01-08 13:29:46 -0500

    Updated from Brython Server: 1/8/2017 1:29:49 PM

--- MMRadar.py
+++ MMRadar.py
@@ -201,12 +201,12 @@ class signal(Sprite):
             if self.s == 1:
                 Sprite(TextAsset(text="Signal Is Strong Enough", width = 200, align = 'center', style = '30px Arial', fill=black),(350,350))
 class plane(Sprite):
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,292-227,125), 1, 'vertical')
+    #asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", Frame(227,0,292-227,125), 1, 'vertical')
+    asset = CircleAsset(5,thinline,black)
     def __init__(self, position):
         super().__init__(plane.asset, position)
         self.rotation = math.pi/2
-        
+        self.center = (
     def step(self):
         self.x -= 0
         
